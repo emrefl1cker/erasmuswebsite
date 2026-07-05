4e027383e056e56c5bcb43d49dd5d47be12e9cb2
emrefl1cker <emreflicker@gmail.com>
2026-07-05 18:51:41 +0300

    Update ayarlar.js

--- src/sanity/ayarlar.js
+++ src/sanity/ayarlar.js
@@ -9,5 +9,4 @@
 export const projectId = "k3kqvj9o";
 export const dataset = "production";
 
-// projectId girilmediyse site data.js'deki yedek içerikle çalışır
 export const cmsAktif = projectId !== "BURAYA_PROJECT_ID" && projectId.length > 3;
